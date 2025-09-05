780c21ccf26f103f469ff343b2ad740bd3fe4f07
Valentin Werner <valwerner2@gmail.com>
2025-09-05 20:36:47 +0200

    nav stack in transaction view

--- TutorTrack/View/TransactionDetailView.swift
+++ TutorTrack/View/TransactionDetailView.swift
@@ -21,102 +21,96 @@ struct TransactionDetailView: View {
     @Query private var pupils: [PupilModel]
     
     var body: some View {
-        VStack(spacing: 0) {
-            Text(transaction.title)
-                .font(.largeTitle)
-            
-            List {
-                Section(
-                    header:
-                        Text("Person")
-                        .font(.headline)
-                        .textCase(nil)
-                ) {
-                    PupilPickerView(transaction: transaction, suggested: Array(pupils[0..<2]))
-                }
-                Section(
-                    header:
-                        Text("Payment")
-                        .font(.headline)
-                        .textCase(nil)
-                ) {
-                    HStack {
-                        Text("Amount").bold()
-                        Spacer()
-                        if editMode?.wrappedValue.isEditing == true {
-                            TextField("Amount", value: $editingTransaction.amount, format: .number
-                            )
-                            .keyboardType(.decimalPad)
-                        } else {
-                            Text(String(transaction.amount))
-                        }
-                    }
-                    HStack {
-                        Text("Type").bold()
-                        Spacer()
-                        if editingTransaction.isBankTransfer != nil {
-                            if editMode?.wrappedValue.isEditing == true {
-                                Toggle("Bank Transfer", isOn: Binding(
-                                    get: { editingTransaction.isBankTransfer! },
-                                    set: { editingTransaction.isBankTransfer = $0 }
-                                ))
-                            }
-                            else if let isBankTransfer = editingTransaction.isBankTransfer {
-                                Text(isBankTransfer ? "Bank Transfer" : "Cash")
-                            }
-                        }
-                        else {
-                            Text("Automatically generated")
-                        }
-                    }
-                }
-                Section(
-                    header:
-                        Text("Additional Info")
-                        .font(.headline)
-                        .textCase(nil)
-                ) {
+        List {
+            Section(
+                header:
+                    Text("Person")
+                    .font(.headline)
+                    .textCase(nil)
+            ) {
+                PupilPickerView(transaction: transaction, suggested: Array(pupils[0..<2]))
+            }
+            Section(
+                header:
+                    Text("Payment")
+                    .font(.headline)
+                    .textCase(nil)
+            ) {
+                HStack {
+                    Text("Amount").bold()
+                    Spacer()
                     if editMode?.wrappedValue.isEditing == true {
-                        HStack {
-                            Text("Name").bold()
-                            Spacer()
-                            TextField("Name", text: $editingTransaction.title)
-                                .focused($nameFieldIsFocused)
+                        TextField("Amount", value: $editingTransaction.amount, format: .number
+                        )
+                        .keyboardType(.decimalPad)
+                    } else {
+                        Text(String(transaction.amount))
+                    }
+                }
+                HStack {
+                    Text("Type").bold()
+                    Spacer()
+                    if editingTransaction.isBankTransfer != nil {
+                        if editMode?.wrappedValue.isEditing == true {
+                            Toggle("Bank Transfer", isOn: Binding(
+                                get: { editingTransaction.isBankTransfer! },
+                                set: { editingTransaction.isBankTransfer = $0 }
+                            ))
+                        }
+                        else if let isBankTransfer = editingTransaction.isBankTransfer {
+                            Text(isBankTransfer ? "Bank Transfer" : "Cash")
                         }
                     }
-                    
+                    else {
+                        Text("Automatically generated")
+                    }
+                }
+            }
+            Section(
+                header:
+                    Text("Additional Info")
+                    .font(.headline)
+                    .textCase(nil)
+            ) {
+                if editMode?.wrappedValue.isEditing == true {
                     HStack {
-                        Text("Note").bold()
+                        Text("Name").bold()
                         Spacer()
-                        if editMode?.wrappedValue.isEditing == true {
-                            TextField("Note", text: $editingTransaction.note)
-                        } else {
-                            Text(transaction.note.isEmpty ? "-" : editingTransaction.note)
-                        }
+                        TextField("Name", text: $editingTransaction.title)
+                            .focused($nameFieldIsFocused)
                     }
                 }
                 
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-            }
-            .onAppear {
-                editingTransaction = transaction
-                if transaction.title.isEmpty
-                {
-                    editMode?.wrappedValue = .active
-                    DispatchQueue.main.async {
-                        nameFieldIsFocused = true
+                HStack {
+                    Text("Note").bold()
+                    Spacer()
+                    if editMode?.wrappedValue.isEditing == true {
+                        TextField("Note", text: $editingTransaction.note)
+                    } else {
+                        Text(transaction.note.isEmpty ? "-" : editingTransaction.note)
                     }
                 }
             }
-            .onDisappear(){
-                update?()
+            
+        }
+        .navigationTitle(transaction.title)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                EditButton()
             }
-            
-            
+        }
+        .onAppear {
+            editingTransaction = transaction
+            if transaction.title.isEmpty
+            {
+                editMode?.wrappedValue = .active
+                DispatchQueue.main.async {
+                    nameFieldIsFocused = true
+                }
+            }
+        }
+        .onDisappear(){
+            update?()
         }
     }
 }
