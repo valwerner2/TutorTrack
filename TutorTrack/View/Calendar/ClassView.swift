2add02ea137c987ef7326ca0d7756193edcf2fef
Valentin Werner <valwerner2@gmail.com>
2025-09-25 17:45:06 +0200

    ClassEditer

--- TutorTrack/View/Calendar/ClassView.swift
+++ TutorTrack/View/Calendar/ClassView.swift
@@ -9,9 +9,60 @@ import SwiftUI
 
 
 struct ClassView: View {
-    let classModel: ClassModel
+    @Bindable var classModel: ClassModel
     
-    var body: some View{
-        Text("hello Class")
+    @Environment(\.editMode) private var editMode
+    @FocusState private var titleFieldIsFocused: Bool
+    
+    var body: some View {
+        List {
+            Section(
+                header:
+                    Text("Basic Data")
+                    .font(.headline)
+                    .textCase(nil)
+            ) {
+                if editMode?.wrappedValue.isEditing == true {
+                    HStack {
+                        Text("Title").bold()
+                        Spacer()
+                        TextField("Title", text: $classModel.title)
+                            .focused($titleFieldIsFocused)
+                    }
+                }
+                HStack {
+                    Text("Note").bold()
+                    Spacer()
+                    if editMode?.wrappedValue.isEditing == true {
+                        TextField("Note", text: $classModel.note)
+                    } else {
+                        Text(classModel.title.isEmpty ? "-" : classModel.title)
+                    }
+                }
+            }
+            Section(
+                header:
+                    Text("Date")
+                    .font(.headline)
+                    .textCase(nil)
+            ){
+                DateIntervalPicker(interval: $classModel.dateInterval)
+            }
+        }
+        .navigationTitle(classModel.title)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                EditButton()
+            }
+        }
+        .onAppear {
+            if classModel.title.isEmpty
+            {
+                editMode?.wrappedValue = .active
+                DispatchQueue.main.async {
+                    titleFieldIsFocused = true
+                }
+            }
+        }
     }
 }
